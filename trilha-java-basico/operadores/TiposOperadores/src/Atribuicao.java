ddec76dd432bde2e8ae3f82a219d9dcfd0ba3e3b
Emi <emi.sabrina16@gmail.com>
2023-01-18 15:42:17 -0300

    Operadores de atribuição

---
+++ trilha-java-basico/operadores/TiposOperadores/src/Atribuicao.java
@@ -0,0 +1,11 @@
+public class Atribuicao {
+    public static void main(String [] args) throws Exception {
+        String nome = "Valentina";
+        int idade = 5;
+        double peso = 12.5;
+        char sexo = 'F';
+        boolean doadoraOrgao = false;
+        /*Date dataNascimento = new Date (); --Novo objeto */
+    }
+        
+}
