c90584805ed9a0a60189ee1a3bc27931c305aa2c
Emi <emi.sabrina16@gmail.com>
2023-01-18 14:59:22 -0300

    exemplos de variaveis

--- trilha-java-basico/tipos-e-variaveis/tipos-variaveis/src/TiposVariaveis.java
+++ trilha-java-basico/tipos-e-variaveis/tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        byte idade = 123;
+         short ano = 2021;
+         int cep =21070333;
+         long cpf = 98765432109L;
+         float pi = 3.14F;
+         final double SALARIO = 1275.33; /*Final não permite que a variavel seja alterada. A variavel deve ser escrita em caixa alta */
+
+         cep = 597000;  /**Pode-se alterar o valor de uma variavel */
+
+         short numeroCurto = 1;
+         int numeroNormal = numeroCurto;
+         short numeroCurto2 = (short) numeroNormal;
+
+         
+         
+
     }
 }
