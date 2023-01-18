6fb4e10e2f496b8f488bf796c9457c26f78e09ee
Emi <emi.sabrina16@gmail.com>
2023-01-18 16:05:37 -0300

    Operadores aritmeticos

---
+++ trilha-java-basico/operadores/TiposOperadores/src/Aritmeticos.java
@@ -0,0 +1,10 @@
+public class Aritmeticos {
+    double soma =10.5 + 15.7;
+    int subtracao = 113 - 25;
+    int multiplicacao = 20 * 7;
+    int divisao = 15 / 3;
+    int modulo = 18 % 3;
+    double resultado = (10 * 7) + (20/4);
+
+    /*O operador de soma, quando utilizado texto faz a junção transformando em uma unica palavra */
+}
