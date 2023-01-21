a41f6b9f0d0507af7169f4ff4f07e40a462d8fa2
Emi <emi.sabrina16@gmail.com>
2023-01-21 15:34:09 -0300

    share

---
+++ POO/visibilidade/src/one/digitalinnovation/oo/Classe2.java
@@ -0,0 +1,14 @@
+package one.digitalinnovation.oo;
+
+
+class Classe2 extends Classe1 {
+
+
+    void metodo() {
+
+        
+        
+    }
+
+
+}
---
+++ POO/visibilidade/src/outro/pacote/Classe4.java
@@ -0,0 +1,16 @@
+package outro.pacote;
+
+import one.digitalinnovation.oo.Classe1;
+
+
+class Classe4 {
+
+    Classe1 classe1;
+
+    void metodo() {
+
+       
+
+    }
+
+}
